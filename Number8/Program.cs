1a6765298ae459787742f3f476ed27c31c4b39d1
Roman3111 <121282733+Roman3111@users.noreply.github.com>
2023-01-19 23:38:50 +0300

    Задача 8

---
+++ Number8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу,которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int everNumber = 2;
+if(number > 1)
+{
+    while(everNumber <= number)
+{
+    Console.Write(everNumber + ",");
+    everNumber = everNumber + 2;
+}
+}
+
