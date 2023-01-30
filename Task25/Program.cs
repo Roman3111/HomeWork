9ddf3d83e89dad74fadf082e40f0e8c0f46bdc53
Roman3111 <121282733+Roman3111@users.noreply.github.com>
2023-01-30 18:44:42 +0300

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень
+System.Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число В ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+for(int i = 1; i < b; i++ )
+{
+    result = result * a;
+}
+System.Console.WriteLine(" А в степени В равно: " + result);
